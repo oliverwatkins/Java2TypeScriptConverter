77636a557f132940689ad3e212ce4763ec9cdbb6
Oliver Watkins <oliver.f.watkins@gmail.com>
2016-08-30 19:20:54 +0200

    asdfasdfasdf

--- src/javaToTypeScript/TypescriptConverter.java
+++ src/javaToTypeScript/TypescriptConverter.java
@@ -83,6 +83,8 @@ public class TypescriptConverter {
 					if (bodyDeclaration instanceof MethodDeclaration) {
 						
 						
+						
+						
 						MethodDeclaration md = (MethodDeclaration)bodyDeclaration;
 						List<Parameter> parameters = md.getParameters();
 						String name = md.getName();
